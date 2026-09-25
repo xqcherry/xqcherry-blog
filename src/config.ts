83511c0f2d697841441c854035b9bf1ec18bd39f
xqcherry <h2417185282@outlook.com>
2026-09-25 13:37:29 +0800

    feat: add guestbook to navigation

--- src/config.ts
+++ src/config.ts
@@ -45,6 +45,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		{
+			name: "留言板",
+			url: "/guestbook/",
+			icon: "material-symbols:chat-bubble-outline-rounded",
+		},
+		{
 			name: "关于",
 			url: "/about/",
 			icon: "material-symbols:person-outline-rounded",
